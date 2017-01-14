feb6284a9b5238660a734ddf9761f9aa9af4e2ac
Robin Daugherty <robin@robindaugherty.net>
2017-01-14 14:23:16 -0500

    Coverage reporting

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in circleci_deployment_notifier.gemspec
 gemspec
+gem 'codacy-coverage', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
+require 'codacy-coverage'
+Codacy::Reporter.start
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "circleci_deployment_notifier"
 
 require 'webmock/rspec'
+WebMock.disable_net_connect! allow: 'api.codacy.com'
