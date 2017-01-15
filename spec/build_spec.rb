0ed4f10061ad2f312125542ae355a8451a0217b8
Robin Daugherty <robin@robindaugherty.net>
2017-01-15 13:54:46 -0500

    Syntax fix

--- spec/build_spec.rb
+++ spec/build_spec.rb
@@ -102,7 +102,7 @@ RSpec.describe CircleciDeploymentNotifier::Build do
 ]
 }
               JSON
-            }
+            },
           )
           send_to_slack
           expect(request_with_body).to have_been_made
